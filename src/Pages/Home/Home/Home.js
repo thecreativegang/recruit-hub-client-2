8f87d519fbc781f7b32319f7e6c6ea6ac51caa1a
Sumonche016 <sumonche016@gmail.com>
2022-07-26 18:55:26 +0600

    service added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import About from '../About/About';
+import Service from '../Service/Service';
 
 const Home = () => {
     return (
         <div>
             <About></About>
+            <Service></Service>
         </div>
     );
 };

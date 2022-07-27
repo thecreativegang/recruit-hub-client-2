f100d7102f08dc7609ddec163e2010b9e2d02898
thecreativegang <thecreativegang503@gmail.com>
2022-07-27 23:07:14 +0600

    app.js updated

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-<<<<<<< HEAD
-import Home from './Pages/Home/Home/Home';
-=======
 import Login from './components/Pages/Login/Login';
 import Register from './components/Pages/Register/Register';
 import Home from './components/Pages/Home/Home';
->>>>>>> cd21153f0c74c8d0922270b77404c875c15ad182
 
 function App() {
   return (
     <div >
-
+      {/*  */}
       <Routes>
         <Route path='/' element={<Home></Home>} />
         <Route path='/home' element={<Home />} />

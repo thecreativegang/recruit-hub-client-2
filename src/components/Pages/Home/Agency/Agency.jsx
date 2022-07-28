63eee030da615f3300911189ab8781db3a890c1a
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-07-28 17:37:31 +0600

    all ok

---
+++ src/components/Pages/Home/Agency/Agency.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Agency.css";
+import { FaRocket } from "react-icons/fa";
+
+const Agency = () => {
+  return (
+    <section>
+      <div className="px-8">
+        {/* Agency header  */}
+        <div className="flex justify-center">
+          <div className="text-center mb-10">
+            <h3 className="section-title font-semibold text-[#5956e9] st-one relative">
+              Agency Statistics
+            </h3>
+            <h1 className="text-[2.6rem] leading-[3.2rem] font-semibold mb-2">
+              Why People’s Like Us
+            </h1>
+            <p className="text-lg font-semibold text-gray-600">
+              Professional Design Agency to provide solutions
+            </p>
+          </div>
+        </div>
+
+        <div className="flex justify-around mt-10 pb-10">
+          <div className="px-6 py-6 text-center inline-block shadow-2xl">
+            <div className="flex justify-center mb-4">
+              <FaRocket className="text-5xl text-[#5956e9]" />
+            </div>
+            <h3 className="text-5xl font-bold mb-2">500+</h3>
+            <p className="text-xl font-semibold text-gray-700">
+              Project Complate
+            </p>
+          </div>
+
+          <div className="px-6 py-6 text-center inline-block shadow-2xl">
+            <div className="flex justify-center mb-4">
+              <FaRocket className="text-5xl text-[#5956e9]" />
+            </div>
+            <h3 className="text-5xl font-bold mb-2">500+</h3>
+            <p className="text-xl font-semibold text-gray-700">
+              Project Complate
+            </p>
+          </div>
+
+          <div className="px-6 py-6 text-center inline-block shadow-2xl">
+            <div className="flex justify-center mb-4">
+              <FaRocket className="text-5xl text-[#5956e9]" />
+            </div>
+            <h3 className="text-5xl font-bold mb-2">500+</h3>
+            <p className="text-xl font-semibold text-gray-700">
+              Project Complate
+            </p>
+          </div>
+
+          <div className="px-6 py-6 text-center inline-block shadow-2xl">
+            <div className="flex justify-center mb-4">
+              <FaRocket className="text-5xl text-[#5956e9]" />
+            </div>
+            <h3 className="text-5xl font-bold mb-2">500+</h3>
+            <p className="text-xl font-semibold text-gray-700">
+              Project Complate
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Agency;
--- src/components/Pages/Home/Experience/Experience.jsx
+++ src/components/Pages/Home/Experience/Experience.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import "./Experience.css";
 
 const Experience = () => {
-    return (
-        <div>
-            
+  return (
+    <>
+      <section className="mt-20 mb-40">
+        <div className="">
+          <div className=" w-full bg-[#5956e9] relative experienceWrap">
+            <div className="w-[50%] text-white py-4 px-8">
+              <h3 className="letsWork text-2xl mt-10 mb-6 inline-block relative z-10 py-4 px-11">
+                Let’s Work
+              </h3>
+              <h1 className="text-[2rem] font-bold mb-12 leading-[3.3rem]">
+                Experience & innovative{" "}
+                <span className="fill-text"> solutions</span> for
+                <span className="fill-text"> creative</span> design &
+                development <span className="fill-text"> agency</span>
+              </h1>
+              <button className="px-4 py-2 text-xs font-semibold rounded-md border-2 hover:bg-black hover:text-white hover:border-black ease-in duration-200 mb-8">
+                {" "}
+                Let’s Work Together{" "}
+              </button>
+            </div>
+
+            {/* image box */}
+            <div className="experienceImg w-[55%] h-[55%] rounded-[8.5rem] absolute bottom-[-18%] right-16 z-20 hidden md:block"></div>
+          </div>
         </div>
-    );
+      </section>
+    </>
+  );
 };
 
 export default Experience;

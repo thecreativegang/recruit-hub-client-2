a6ab098481529d1db7ffb4d9274936627a27a5f2
SouravBiswas238 <souravbiswas427@gmail.com>
2022-07-29 21:02:11 +0600

    adding soket forntend

--- src/components/Pages/ChatPage/ChatPage.js
+++ src/components/Pages/ChatPage/ChatPage.js
@@ -2,23 +2,49 @@ import axios from 'axios';
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import ChatBox from './ChatBox';
+import MyChat from './MyChat';
+import SideDrawer from './SideDrawer';
+
 
 const ChatPage = () => {
 
     const [chats, setChats] = useState([]);
     const fetchChats = async () => {
-        const data = await axios.get(`http://localhost:5000/api/chat`);
+        const data = await axios.get(`http://localhost:3001/api/chat`);
         setChats(data.data);
     }
     useEffect(() => {
         fetchChats();
     }, [])
-    console.log(chats)
     return (
         <div>
-            {chats?.map((chat) => (
-                <div>Name:  {chat?.chatName}</div>
-            ))}
+
+            <div class="drawer">
+                <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+                <div class="drawer-content">
+
+                    <div className='grid grid-cols-3'>
+                        <div className=''>
+                            <MyChat chats={chats}></MyChat>
+                        </div>
+                        <div className='col-span-2'>
+                            <ChatBox></ChatBox>
+                        </div>
+                    </div>
+                </div>
+                <div class="drawer-side">
+                    <label for="my-drawer" class="drawer-overlay"></label>
+                    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+                        <li className='text-center'>Search User</li>
+
+                        <div className=' flex'>
+                            <input className='border-white-100 border-2 w-[70%]' type="text" placeholder='search' />
+                            <span className='btn-sm btn'> go</span>
+                        </div>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };

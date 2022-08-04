dde924de27a001bdf9fbd9c559fc23e54c0be593
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-08-04 15:45:36 +0600

    added logiin register

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -5,17 +5,21 @@ import './Navbar.css'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import { signOut } from 'firebase/auth';
+
+// react icon import links 
 import { FaUserCircle } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { BsPlusLg, BsTools, BsInfoCircleFill } from "react-icons/bs";
+import { BsPlusLg, BsTools, BsInfoCircleFill, BsFillFileEarmarkMedicalFill } from "react-icons/bs";
 import { MdDeveloperMode, MdOutlineConnectWithoutContact } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 import { AiFillHome } from "react-icons/ai";
+import { RiLoginBoxLine } from "react-icons/ri";
 
 const Navbar = () => {
     const [navSwitch, setNavSwitch] = useState(false);
 
     const [user] = useAuthState(auth);
+
     const handleSignOut = () => {
         localStorage.removeItem('accessToken')
         signOut(auth);
@@ -27,18 +31,18 @@ const Navbar = () => {
 
     return (
         <nav className='sticky top-0 left-0 z-50 shadow-2xl bg-gradient-to-r from-[#e6e6ff] to-[#d6fff5] '>
-            <div className='container max-w-[1280px] mx-auto relative z-50 '>
+            <div className='container max-w-[1280px] mx-auto relative z-50'>
                 <div className='flex justify-between items-center relative'>
                     {/* logo and navbar menu */}
-                    <div className='logo flex items-center text-2xl font-bold py-3 ml-3'>
-                        {/* navbar menu  */}
+                    <div className='logo flex items-center text-xl font-bold py-3 ml-3'>
+                        {/* navbar menu switch */}
                         <div onClick={() => setNavSwitch(!navSwitch)} className='mr-2 text-2xl md:hidden ease-out duration-200 '>
                             {
                                 !navSwitch ? <GiHamburgerMenu /> : <BsPlusLg className='rotate-45' />
                             }
 
                         </div>
-                        <img className='w-10 h-10' src={navLogo} alt="Logo" />
+                        <img className='w-8 h-8' src={navLogo} alt="Logo" />
                         <span className='ml-1'>RecruitHub</span>
                     </div>
 
@@ -47,26 +51,46 @@ const Navbar = () => {
 
                         <ul className='md:flex md:items-center  bg-gradient-to-r from-[#eaeaff] to-[#e4fff9] md:bg-gradient-to-r md:from-[#eaeaff00] md:to-[#e4fff900] pl-3 md:pl-0 py-5 md:py-0 w-full md:w-auto'>
 
-                            <li className='mr-8 mb-4 md:mb-0'> <NavLink to="/" className={({ isActive }) => (isActive ? active : deActive)} ><AiFillHome className='md:hidden text-base mr-2' />Home</NavLink> </li>
+                            <li className='mr-5 mb-4 md:mb-0'> <NavLink to="/" className={({ isActive }) => (isActive ? active : deActive)} ><AiFillHome className='md:hidden text-base mr-2' />Home</NavLink> </li>
 
-                            <li className='mr-8 mb-4 md:mb-0'> <NavLink to="/service" className={({ isActive }) => (isActive ? active : deActive)} ><BsTools className='md:hidden text-base mr-2' /> Services</NavLink> </li>
+                            <li className='mr-5 mb-4 md:mb-0'> <NavLink to="/service" className={({ isActive }) => (isActive ? active : deActive)} ><BsTools className='md:hidden text-base mr-2' /> Services</NavLink> </li>
 
-                            <li className='mr-8 mb-4 md:mb-0'> <NavLink to="/developer" className={({ isActive }) => (isActive ? active : deActive)} > <MdDeveloperMode className='md:hidden text-base mr-2' /> Developer</NavLink> </li>
+                            <li className='mr-5 mb-4 md:mb-0'> <NavLink to="/developer" className={({ isActive }) => (isActive ? active : deActive)} > <MdDeveloperMode className='md:hidden text-base mr-2' /> Developer</NavLink> </li>
 
 
-                            <li className='mr-8 mb-4 md:mb-0'> <NavLink to="/contactUs" className={({ isActive }) => (isActive ? active : deActive)} > <MdOutlineConnectWithoutContact className='md:hidden text-base mr-2' /> Contact US</NavLink> </li>
+                            <li className='mr-5 mb-4 md:mb-0'> <NavLink to="/contactUs" className={({ isActive }) => (isActive ? active : deActive)} > <MdOutlineConnectWithoutContact className='md:hidden text-base mr-2' /> Contact US</NavLink> </li>
 
-                            <li className='mr-8 mb-4 md:mb-0'> <NavLink to="/aboutUs" className={({ isActive }) => (isActive ? active : deActive)} > <BsInfoCircleFill className='md:hidden text-base mr-2' /> About US</NavLink> </li>
+                            <li className='mb-4 md:mb-0'> <NavLink to="/aboutUs" className={({ isActive }) => (isActive ? active : deActive)} > <BsInfoCircleFill className='md:hidden text-base mr-2' /> About US</NavLink> </li>
+
+
+                            {/* profile routes show only mobile and tablet  */}
+                            {
+                                !user &&
+                                <>
+                                    <li className='mb-4 md:mb-0 md:hidden'> <NavLink to="/login" className={({ isActive }) => (isActive ? active : deActive)} > <RiLoginBoxLine className='md:hidden text-base mr-2' />Login</NavLink> </li>
+
+                                    <li className='mb-4 md:mb-0 md:hidden'> <NavLink to="/register" className={({ isActive }) => (isActive ? active : deActive)} > <BsFillFileEarmarkMedicalFill className='md:hidden text-base mr-2' />Register</NavLink> </li>
+                                </>
+                            }
+
+
                         </ul>
 
                     </div>
 
                     {/* profile  */}
                     <div className='profile'>
-                        <ul className='flex items-center'>
-                            <li className='mr-5 text-lg font-semibold'>Login</li>
-                            <li className='mr-5 text-lg font-semibold'>Sing In</li>
-                            <FaUserCircle className='text-4xl mr-3 hidden' />
+                        <ul className='flex items-center '>
+
+                            {!user ? <>
+                                <li className='mr-4 mb-4 md:mb-0 hidden md:block'> <NavLink to="/login" className={({ isActive }) => (isActive ? active : deActive)} >Login</NavLink> </li>
+
+                                <li className='mr-4 mb-4 md:mb-0 hidden md:block'> <NavLink to="/register" className={({ isActive }) => (isActive ? active : deActive)} >Register</NavLink> </li></> :
+
+
+                                <FaUserCircle className='text-4xl mr-3' />
+                            }
+
                         </ul>
                     </div>
 

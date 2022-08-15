198fa540804538f82136600a56173bd4f5c602a2
Sumon Bala <Sumonche016@gmail.com>
2022-08-16 01:13:46 +0600

    review page ui and user data collection ready

--- src/components/Pages/Review/Review.js
+++ src/components/Pages/Review/Review.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import './Review.css'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import { useForm } from 'react-hook-form';
 const Review = () => {
-    const [user, loading, error] = useAuthState(auth);
-    console.log(user)
+
+    const [user] = useAuthState(auth);
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+
+
+
     return (
-        <div>
-            <h1>From review</h1>
+
+        <div className=' page-bg'>
+            <div className='py-24'>
+                <div className='w-1/2 mx-auto p-12 bg-white review-shadow'>
+                    <div className=' flex justify-center '>
+                        <h1 className='section-tittle relative color-blue-dark font-semibold inline-block  text-center'>Review</h1>
+
+                    </div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <input value={user?.displayName} className='review-input mb-4' {...register("firstName")} />
+                        <input placeholder='Your Review 1 to 5' required min={1} max={5} type='number' className='review-input mb-4' {...register("rating")} />
+                        <textarea required="" name="" id="" class="review-textarea mb-4 h-[150px] w-[100%]" placeholder="Tell Me about Us Freely " spellcheck="false" autocomplete="off" {...register("review-text")}></textarea>
+                        <input className='bg-blue-dark py-[13px] px-[35px] font-semibold cursor-pointer text-white' type="submit" />
+                    </form>
+                </div>
+            </div>
+
         </div>
     );
 };

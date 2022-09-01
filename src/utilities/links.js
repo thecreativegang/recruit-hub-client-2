1bd89f2747b319dca3127737f4b544b0950cb2c2
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-09-01 20:36:49 +0600

    pull

--- src/utilities/links.js
+++ src/utilities/links.js
@@ -1,1 +1,2 @@
-export const serverLink = `https://safe-oasis-01130.herokuapp.com`
+// export const serverLink = `https://safe-oasis-01130.herokuapp.com`
+export const serverLink = `http://localhost:3000`

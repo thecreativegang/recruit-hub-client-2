56a95ac1d895f488873ffccfcef68015352ffadf
hasibmiraz <hasibmiraz09@gmail.com>
2022-09-02 17:50:28 +0600

    Fixed typos

--- src/components/Pages/UserProfile/UserSkills/UserSkills.jsx
+++ src/components/Pages/UserProfile/UserSkills/UserSkills.jsx
@@ -1,68 +1,68 @@
-import React from "react";
-import "./UserSkills.css";
+import React from 'react';
+import './UserSkills.css';
 
-import { FaHtml5 } from "react-icons/fa";
-import { BiMessageSquareError } from "react-icons/bi";
-import { BsCheckCircle } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { FaHtml5 } from 'react-icons/fa';
+import { BiMessageSquareError } from 'react-icons/bi';
+import { BsCheckCircle } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 const UserSkills = () => {
   const userInfo = {
     CoverPhoto:
-      "https://i.pinimg.com/564x/30/5c/5a/305c5a457807ba421ed67495c93198d3.jpg",
+      'https://i.pinimg.com/564x/30/5c/5a/305c5a457807ba421ed67495c93198d3.jpg',
     ProfilePhoto:
-      "https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg",
-    name: "Tanvir Ahmed",
-    userName: "tkljfld.232",
-    developerType: "Front-End Web Developer",
-    Bio: "Front-End Web Developer | Interested in React Js. and React Native | Passionate about Web Development | Dreaming to be a Professional Full Stack Web Developer",
-    state: "barisal",
-    country: "bangladesh",
-    contactsInfo: { email: "", phone: "" },
-    socialLink: { github: "", linkdin: "", facebook: "", instagram: "" },
+      'https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg',
+    name: 'Tanvir Ahmed',
+    userName: 'tkljfld.232',
+    developerType: 'Front-End Web Developer',
+    Bio: 'Front-End Web Developer | Interested in React Js. and React Native | Passionate about Web Development | Dreaming to be a Professional Full Stack Web Developer',
+    state: 'barisal',
+    country: 'bangladesh',
+    contactsInfo: { email: '', phone: '' },
+    socialLink: { github: '', linkdin: '', facebook: '', instagram: '' },
     skills: [
-      { skillName: "HTMl 5", skillTest: 100 },
-      { skillName: "CSS 3", skillTest: 80 },
-      { skillName: "Javascript", skillTest: 79 },
+      { skillName: 'HTMl 5', skillTest: 100 },
+      { skillName: 'CSS 3', skillTest: 80 },
+      { skillName: 'Javascript', skillTest: 79 },
     ],
     featured: [
-      { featuredPhoto: "", featuredTitle: "", featuredDescription: "" },
-      { featuredPhoto: "", featuredTitle: "", featuredDescription: "" },
-      { featuredPhoto: "", featuredTitle: "", featuredDescription: "" },
+      { featuredPhoto: '', featuredTitle: '', featuredDescription: '' },
+      { featuredPhoto: '', featuredTitle: '', featuredDescription: '' },
+      { featuredPhoto: '', featuredTitle: '', featuredDescription: '' },
     ],
     experince: [
       {
-        experinceTitle: "",
-        jobType: "full time",
-        duration: "",
-        skills: ["", "", "", ""],
+        experinceTitle: '',
+        jobType: 'full time',
+        duration: '',
+        skills: ['', '', '', ''],
       },
     ],
     courses: [
-      { coursesPhoto: "", coursesTitle: "", coursesDescription: "" },
-      { coursesPhoto: "", coursesTitle: "", coursesDescription: "" },
-      { coursesPhoto: "", coursesTitle: "", coursesDescription: "" },
+      { coursesPhoto: '', coursesTitle: '', coursesDescription: '' },
+      { coursesPhoto: '', coursesTitle: '', coursesDescription: '' },
+      { coursesPhoto: '', coursesTitle: '', coursesDescription: '' },
     ],
     projects: [
       {
-        projectsPhoto: "",
-        projectsTitle: "",
-        projectsDescription: "",
-        projectsLink: { githubServer: "", githubClint: "", liveSite: "" },
+        projectsPhoto: '',
+        projectsTitle: '',
+        projectsDescription: '',
+        projectsLink: { githubServer: '', githubClint: '', liveSite: '' },
       },
 
       {
-        projectsPhoto: "",
-        projectsTitle: "",
-        projectsDescription: "",
-        projectsLink: { githubServer: "", githubClint: "", liveSite: "" },
+        projectsPhoto: '',
+        projectsTitle: '',
+        projectsDescription: '',
+        projectsLink: { githubServer: '', githubClint: '', liveSite: '' },
       },
 
       {
-        projectsPhoto: "",
-        projectsTitle: "",
-        projectsDescription: "",
-        projectsLink: { githubServer: "", githubClint: "", liveSite: "" },
+        projectsPhoto: '',
+        projectsTitle: '',
+        projectsDescription: '',
+        projectsLink: { githubServer: '', githubClint: '', liveSite: '' },
       },
     ],
   };
@@ -82,12 +82,12 @@ const UserSkills = () => {
               {parseInt(skill?.skillTest) >= 80 ? (
                 <p className="text-base font-medium text-green-600 flex justify-start items-center pt-2">
                   <BsCheckCircle className="mr-2" />
-                  VERIFY BY RECRUIT HUB
+                  VERIFIED BY RECRUIT HUB
                 </p>
               ) : (
                 <>
                   <p className="text-base font-medium text-yellow-500 flex justify-start items-center pt-2 mb-3">
-                    <BiMessageSquareError className="mr-2" /> NO VERIFY
+                    <BiMessageSquareError className="mr-2" /> NO VERIFIED YET
                   </p>
                   <Link
                     to="#"

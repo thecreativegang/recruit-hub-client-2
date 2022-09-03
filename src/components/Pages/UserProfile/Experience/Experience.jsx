a0c2168088b74a714c86049817b4b2c6f28a5f7c
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-09-04 00:44:22 +0600

    added project

--- src/components/Pages/UserProfile/Experience/Experience.jsx
+++ src/components/Pages/UserProfile/Experience/Experience.jsx
@@ -1,99 +1,67 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../../firebase.init";
+import { serverLink } from "../../../../utilities/links";
+import SpinLoading from "../../../Shared/SpinLoading/SpinLoading";
 import "./Experience.css";
 
 const Experience = () => {
-  const userInfo = {
-    CoverPhoto:
-      "https://i.pinimg.com/564x/30/5c/5a/305c5a457807ba421ed67495c93198d3.jpg",
-    ProfilePhoto:
-      "https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg",
-    name: "Tanvir Ahmed",
-    userName: "tkljfld.232",
-    developerType: "Front-End Web Developer",
-    Bio: "Front-End Web Developer | Interested in React Js. and React Native | Passionate about Web Development | Dreaming to be a Professional Full Stack Web Developer",
-    state: "barisal",
-    country: "bangladesh",
-    contactsInfo: { email: "", phone: "" },
-    socialLink: { github: "", linkdin: "", facebook: "", instagram: "" },
-    skills: [
-      { skillName: "", skillTest: 100 },
-      { skillName: "", skillTest: "" },
-      { skillName: "", skillTest: "" },
-    ],
-    featured: [
-      { featuredPhoto: "", featuredTitle: "", featuredDescription: "" },
-      { featuredPhoto: "", featuredTitle: "", featuredDescription: "" },
-      { featuredPhoto: "", featuredTitle: "", featuredDescription: "" },
-    ],
-    experince: [
-      {
-        experincePhoto: "adsfas",
-        experinceTitle: "asdf",
-        jobType: "full time",
-        duration: "afdas",
-        skills: ["asfd", "affdsa", "sdfs", "afdas"],
-      },
-      {
-        experinceTitle: "gfdgf",
-        jobType: "full time",
-        duration: "dsfsafs",
-        skills: ["kjah", "sdf", "", "adfa"],
-      },
-    ],
-    courses: [
-      { coursesPhoto: "", coursesTitle: "", coursesDescription: "" },
-      { coursesPhoto: "", coursesTitle: "", coursesDescription: "" },
-      { coursesPhoto: "", coursesTitle: "", coursesDescription: "" },
-    ],
-    projects: [
-      {
-        projectsPhoto: "",
-        projectsTitle: "",
-        projectsDescription: "",
-        projectsLink: { githubServer: "", githubClint: "", liveSite: "" },
-      },
+  const [userInfo, serUserInfo] = useState({});
 
-      {
-        projectsPhoto: "",
-        projectsTitle: "",
-        projectsDescription: "",
-        projectsLink: { githubServer: "", githubClint: "", liveSite: "" },
-      },
+  const [loading, serLoading] = useState(true);
 
-      {
-        projectsPhoto: "",
-        projectsTitle: "",
-        projectsDescription: "",
-        projectsLink: { githubServer: "", githubClint: "", liveSite: "" },
-      },
-    ],
-  };
+  const [user] = useAuthState(auth);
+  const email = user?.email;
 
-  return (
+  useEffect(() => {
+    axios
+      .get(`${serverLink}/user/email/${email}`, {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+      .then((res) => {
+        if (res?.data) {
+          serUserInfo(res?.data);
+          serLoading(false);
+        }
+      });
+  }, [email]);
+
+  console.log(userInfo);
+  return loading ? (
+    <SpinLoading />
+  ) : (
     <section>
       <h2 className="user-title">Our Experience</h2>
       <div className="px-5 pb-5">
         {/* all Experience box */}
         <div>
-          {userInfo?.experince.map((singleExperince) => (
-            <div className="max-w-[25rem] w-full px-2 py-2 user-card-shadow rounded mb-10 flex justify-start gap-3 align-top">
+          {userInfo?.experience.map((singleExperience, index) => (
+            <div
+              key={index}
+              className="max-w-[25rem] w-full px-2 py-2 user-card-shadow rounded mb-10 flex justify-start gap-3 align-top"
+            >
               <div>
                 <img
                   className="w-12 h-12 object-cover border-2 border-gray-200"
-                  src={singleExperince?.experincePhoto}
+                  src={singleExperience?.experience_photo}
                   alt="Experience Images"
                 />
               </div>
               <div>
                 <h3 className="text-lg font-medium capitalize">
-                  {singleExperince?.experinceTitle}
+                  {singleExperience?.experience_title}
                 </h3>
-                <p className="">{singleExperince?.jobType}</p>
-                <p className="">{singleExperince?.duration}</p>
+                <p className="">{singleExperience?.jobType}</p>
+                <p className="">{singleExperience?.duration}</p>
                 <p className="font-medium text-gray-600 mt-1">
                   Skills:{" "}
-                  {singleExperince?.skills.map((skill) => (
-                    <span> {skill}</span>
+                  {singleExperience?.skills.map((skill, index) => (
+                    <span key={index}> {skill}</span>
                   ))}
                 </p>
               </div>
